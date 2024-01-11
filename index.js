3db85331dfcd93dbf5fe353e66fbdb7edc5163f0
Anna Kioko <annakioko00@gmail.com>
2024-01-11 07:36:13 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function superbowlWin(record) {
+    const winningGame = record.find(game => game.result === "W");
+  
+    if (winningGame) {
+      return winningGame.year;
+    } else {
+      return undefined;
+    }
+  }
+
+  const games = [
+    { year: 2019, result: "L" },
+    { year: 2020, result: "W" },
+    { year: 2021, result: "L" },
+  ];
+  
+  console.log(superbowlWin(games)); 
+  
